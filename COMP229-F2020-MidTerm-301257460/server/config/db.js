bb54c11efb553303541044ebd0b87932023aad48
Mahsa Dorri <mahsadorri093@gmail.com>
2023-03-02 17:56:00 -0500

    Last version of midterm

---
+++ COMP229-F2020-MidTerm-301257460/server/config/db.js
@@ -0,0 +1,11 @@
+/* 
+Page: db.js
+Name: Mahsa Dorri
+Student Id: 301257460
+Date:0./0./2023
+*/
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://mahsad:GKCkyj3TDSAXoW7z@mongodbserver.v4qnsye.mongodb.net/book_store?retryWrites=true&w=majority"
+};
